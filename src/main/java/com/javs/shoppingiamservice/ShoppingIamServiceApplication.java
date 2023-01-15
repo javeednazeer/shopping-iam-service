b2af3563233b0f194ec812b048099c6c0e4b7a4d
javeednazeer <javeednazeer.tech@gmail.com>
2023-01-15 15:02:52 +0800

    first commit

---
+++ src/main/java/com/javs/shoppingiamservice/ShoppingIamServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javs.shoppingiamservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingIamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingIamServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javs/shoppingiamservice/ShoppingIamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javs.shoppingiamservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingIamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
